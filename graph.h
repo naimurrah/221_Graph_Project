0c57697974ed070978a6b34bef5aa2381f67b00e
Uchenna Akahara <akaharauchenna@gmail.com>
2022-05-06 16:41:35 -0500

    Adding graph.h file to repo. This file is where all the changes will be made for the Graph ADT

---
+++ graph.h
@@ -0,0 +1,60 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <iostream>
+#include <queue>
+
+class Graph {
+private:
+    // TODO(student): implement graph
+    struct Vertex{
+        /*
+        each verte holds a value
+        a vertex knows its neighbours, to do this, use a vector<Vertex>
+        a vertex also has an edge, and an edge is simple a source and a destination
+        */
+        size_t val;
+        bool known;
+        size_t distance;
+        
+
+        // std::unordered_set<Vertex> neighbor;
+
+
+    };
+
+public:
+    // Task 1
+    Graph();
+    Graph(const Graph& other);
+    Graph& operator=(const Graph& other);
+    ~Graph();
+
+    size_t vertex_count() const;
+    size_t edge_count() const;
+
+    bool contains_vertex(size_t id) const;
+    bool contains_edge(size_t src, size_t dest) const;
+    double cost(size_t src, size_t dest) const;
+
+    bool add_vertex(size_t id);
+    bool add_edge(size_t src, size_t dest, double weight=1);
+    bool remove_vertex(size_t id);
+    bool remove_edge(size_t src, size_t dest);
+
+    // Task 2
+    void prim(size_t source_id);
+    bool is_path(size_t id) const;
+    void print_path(size_t dest_id, std::ostream& os=std::cout) const;
+
+    // Task 3
+    void dijkstra(size_t source_id);
+    double distance(size_t id) const;
+    void print_shortest_path(size_t dest_id, std::ostream& os=std::cout) const;
+};
+
+#endif  // GRAPH_H
