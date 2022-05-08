c736556229c0403375eeae26b1afe660a4860df8
Uchenna <akaharauchenna@gmail.com>
2022-05-07 19:56:17 -0500

    Graph updates. Collaborative session
    Co-authored-by: naimurrah <naimurrah@users.noreply.github.com>

--- graph.h
+++ graph.h
@@ -8,96 +8,231 @@
 #include <iostream>
 #include <queue>
 
-class Graph {
+class Graph
+{
 private:
     // TODO(student): implement graph
-    struct Edge;
+    // struct Edge;
 
-    struct Vertex{
+    struct Vertex
+    {
         /*
         each verte holds a value
         a vertex knows its neighbours, to do this, use a vector<Vertex>
         a vertex also has an edge, and an edge is simple a source and a destination
         */
         size_t id;
-        std::list<Vertex> neighbors;
-        std::list<Edge> edges;
 
-        //pretty sure these are used during traversal.
+        size_t index;
+        std::vector<Vertex *> neighbors;
+        // std::vector<Edge> edges;
+
+        // pretty sure these are used during traversal.
         bool visited;
-        size_t distance;
-        Vertex* previous;       
+        int distance;
+        Vertex *previous;
+        // &Edge find edge(Vertex destination) // returns an edge with this being point a and destination point b
 
         // vertex constructor
-        Vertex(size_t i, std::list<Vertex> n, std::list<Edge> e, bool v, size_t d, Vertex* p = nullptr) : id(i), neighbors(n), edges(e), visited(v), distance(d), previous(p){}
+        Vertex(size_t i, size_t in, size_t d = std::numeric_limits<int>::max(), bool v = false, Vertex *p = nullptr) : id(i), index(in), visited(v), distance(d), previous(p) {}
     };
 
-// Struct for an edge
-/*
-An edge contains its start and end points and the weight of the edge as well.
+    // Struct for an edge
+    /*
+    An edge contains its start and end points and the weight of the edge as well.
 
-*/
-    struct Edge{
+    */
+    /*
+    struct Edge
+    {
         Vertex start;
         Vertex end;
         size_t weight;
-
-        Edge(Vertex s, Vertex e, size_t w) : start(s), end(e), weight(w){}
+        Edge(Vertex s, Vertex e, size_t w) : start(s), end(e), weight(w) {}
     };
+    */
 
     // unordered set to keep count of vertices presenent
-    std::unordered_set <size_t> id_set; // using this so when using contains we could easly check if its in the set. Each id will be unique
+    std::unordered_set<size_t> id_set; // using this so when using contains we could easly check if its in the set. Each id will be unique
     // List of some sort to hold the vertices in the graph
-    std::list<Vertex> vertices;
+    std::vector<Vertex *> vertices;
+
+    // Maybe add -
+    std::vector<std::vector<size_t>> weights;
+    /*0
+    0 0
+    */
     size_t v_count; // vertex count
     size_t e_count; // edge count
+    size_t find_vertex_index(size_t id) const
+    {
+        for (Vertex *v : vertices)
+        {
+            if (v->id == id)
+            {
+                return v->index;
+            }
+        }
+        return 10000000;
+    }
+
 public:
     // Task 1
     // start
-    Graph(): v_count(0), e_count(0){};
-    Graph(const Graph& other){
-        if(other.vertices.size() == 0){
+    Graph() : v_count(0), e_count(0){};
+    Graph(const Graph &other)
+    {
+        if (other.vertices.size() == 0)
+        {
             this->v_count = 0;
             this->e_count = 0;
             return;
         }
         this->id_set = other.id_set;
         this->vertices = other.vertices;
+        this->weights = other.weights;
         this->v_count = other.v_count;
         this->e_count = other.e_count;
 
-      // need to think more but this is where   
-        
-
+        // need to think more but this is where
     };
-    Graph& operator=(const Graph& other);
-    ~Graph();
+    Graph &operator=(const Graph &other)
+    {
+        if (this != &other)
+        {
+            this->id_set = other.id_set;
+            this->vertices = other.vertices;
+            this->weights = other.weights;
+            this->v_count = other.v_count;
+            this->e_count = other.e_count;
+        }
+        return *this;
+    };
+    ~Graph()
+    {
+        id_set.clear();
+        vertices.clear();
+        weights.clear();
+        v_count = 0;
+        e_count = 0;
+    };
 
-    size_t vertex_count() const{
+    size_t vertex_count() const
+    {
         return v_count;
     };
-    size_t edge_count() const{
+    size_t edge_count() const
+    {
         return e_count;
     };
 
-    bool contains_vertex(size_t id) const;
-    bool contains_edge(size_t src, size_t dest) const;
-    double cost(size_t src, size_t dest) const;
+    bool contains_vertex(size_t id) const
+    {
+        return !(id_set.find(id) == id_set.end());
+    }
 
-    bool add_vertex(size_t id);
-    bool add_edge(size_t src, size_t dest, double weight=1);
-    bool remove_vertex(size_t id);
-    bool remove_edge(size_t src, size_t dest);
+    bool contains_edge(size_t src, size_t dest) const
+    {
+        size_t src_i = find_vertex_index(src);
+        size_t dest_i = find_vertex_index(dest);
+
+        return weights[src_i][dest_i] != 0;
+    }
+
+    double cost(size_t src, size_t dest) const
+    {
+        if (weights[src][dest] == 0)
+            return std::numeric_limits<int>::max();
+        return weights[src][dest];
+    };
+
+    /*0 1
+    0 0 0
+    1 2 0
+    */
+    bool add_vertex(size_t id)
+    {
+
+        if (contains_vertex(id))
+        {
+            return false;
+        }
+
+        Vertex *new_vertex = new Vertex(id, v_count);
+        vertices.emplace_back(new_vertex);
+        std::vector<size_t> newRow(v_count + 1);
+        weights.emplace_back(newRow);
+        id_set.insert(id);
+        
+        for (size_t i = 0; i < weights.size() - 1; i++)
+        {
+            weights[i].resize(v_count + 1);
+        }
+
+        v_count++;
+        return true;
+    }
+
+    bool add_edge(size_t src, size_t dest, double weight = 1)
+    {
+        if (contains_edge(src, dest))
+        {
+            return false;
+        }
+        size_t src_i = find_vertex_index(src);
+        size_t dest_i = find_vertex_index(src);
+        weights[src_i][dest_i] = weight;
+        return true;
+    }
+
+    bool remove_vertex(size_t id) {
+        if (!contains_vertex(id)) {
+            return false;
+        }
+        size_t index = find_vertex_index(id);
+        id_set.erase(id_set.find(id));
+
+    
+        Vertex* del = std::move(vertices[index]);
+        delete del;
+
+        for (size_t i = index; i < vertices.size()-1; i++) {
+            vertices[i] = std::move(vertices[i+1]);
+            vertices[i]->index = vertices[i]->index - 1; 
+        }
+        vertices.pop_back();
+        weights.pop_back();
+        
+        for (size_t i = 0; i < weights.size(); i++) {
+            auto remove = weights[i].begin() + index;
+            weights[i].erase(remove);
+        }
+        
+        v_count--;
+        return true;
+    }
+    
+    bool remove_edge(size_t src, size_t dest){
+        if(!contains_edge(src, dest)){
+            return false;
+        }
+        size_t src_i = find_vertex_index(src);
+        size_t dest_i = find_vertex_index(dest);
+        
+        weights[src_i][dest_i] = 0;
+        e_count--;
+        return true;        
+    };
 
     // Task 2
     void prim(size_t source_id);
     bool is_path(size_t id) const;
-    void print_path(size_t dest_id, std::ostream& os=std::cout) const;
+    void print_path(size_t dest_id, std::ostream &os = std::cout) const;
 
     // Task 3
     void dijkstra(size_t source_id);
     double distance(size_t id) const;
-    void print_shortest_path(size_t dest_id, std::ostream& os=std::cout) const;
+    void print_shortest_path(size_t dest_id, std::ostream &os = std::cout) const;
 };
 
-#endif  // GRAPH_H
+#endif // GRAPH_H
